8921c40a65a60468b66b269c3825b7d58bf62870
“Wai <waye14671@gmail.com>
2025-12-18 23:16:09 +0630

    heroSection

--- js/index.js
+++ js/index.js
@@ -5,24 +5,21 @@ const mobileNavToggleLine3 =  document.querySelector(".mobile-nav-toggle-line-3"
 const mobileNavToggleLine1 =  document.querySelector(".mobile-nav-toggle-line-1");
 
 
-mobileNavToggleBtn.addEventListener("click" , () => {
-    
+function toggleMobileNav(){
     mobileNavLinks.classList.toggle("active-mobile-nav-links");
 
     // annimation 
     mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line-2");
-    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line-3");
     mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line-1");
-    
+    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line-3");
+}
+mobileNavToggleBtn.addEventListener("click" , ()=> {
+    toggleMobileNav();
 })
 
-mobileNavLinks.addEventListener("click" , (e) => {
-    if(e.target.tagName = "a"){
-        mobileNavLinks.classList.toggle("active-mobile-nav-links");
 
-        // annimation 
-        mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line-2");
-        mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line-1");
-        mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line-3");
+mobileNavLinks.addEventListener("click" , (e) => {
+    if(e.target.tagName === "A"){
+       toggleMobileNav();
     }
 })
